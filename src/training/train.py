de61f19a07bbf3df5b72a9367f351aa5a027075f
Jacek Sikora <jack.sikor@gmail.com>
2026-04-04 11:56:22 +0200

    change training configuration to train on all available data

--- src/training/train.py
+++ src/training/train.py
@@ -1,8 +1,6 @@
 import joblib
 from sentence_transformers import SentenceTransformer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
-from sklearn.model_selection import train_test_split
 
 
 def train_model():
@@ -18,15 +16,10 @@ def train_model():
     labels = [2, 0, 1, 2, 0, 1]
 
     model = SentenceTransformer("all-MiniLM-L6-v2")
-
     X = model.encode(texts)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, labels, test_size=0.2)
-
     clf = LogisticRegression()
-    clf.fit(X_train, y_train)
-
-    print(classification_report(y_test, clf.predict(X_test)))
+    clf.fit(X, labels)
 
     model.save("models/embedding_model")
     joblib.dump(clf, "models/sentiment_model.joblib")
